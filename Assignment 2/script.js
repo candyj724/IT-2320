48a84880920cca8c87fc878dfc95d95c836004ac
candyj724@att.net <1manager12>
2017-03-24 20:31:02 -0400

    Assignment 2
    unobstrusive javascript

---
+++ Assignment 2/script.js
@@ -0,0 +1,12 @@
+< script >
+  function myFunction() {
+    document.getElementById("demo").innerHTML = "Music make the round go round";
+  } < /script> < script >
+
+function MouseOverListItem(li) {
+  li.className = "list-item-highlight";
+}
+var myFavoriteSong =
+  document.getElementById("song");
+document.getElementById("group1").InnerHTML =
+  "My favorite Nee Edition song is " + myFavoriteSong.innerHTML; < /script>
